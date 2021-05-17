8eb183919d01e43e4ca385de1f7673566689d6dc
zerinriz <zerin_95@hotmail.com>
2021-05-17 17:58:33 +0200

    Todo list

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([])
+  return (
+    <div className="App">
+      <header>
+        <h1>ToDo List</h1>
+      </header>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+}
+
+export default App;

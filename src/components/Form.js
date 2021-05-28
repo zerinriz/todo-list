66d5e73a909ae2fd85087522c4ae41803b683014
zerinriz <zerin_95@hotmail.com>
2021-05-28 23:12:58 +0200

    Izmjene

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Form = ({ setInputText, todos, setTodos, inputText }) => {
-    const inputTextHandler = (e) => {
-        console.log(e.target.value);
-        setInputText(e.target.value);
-    };
-    const submitTodoHandler = (e) => {
-        e.preventDefault();
-        setTodos([
-            ...todos,
-            { text: inputText, completed: false, id: Math.random() * 1000 }
-        ]);
-        setInputText("");
-    };
-    return (
-        <form onSubmit={submitTodoHandler}>
-            <input id="myText" value={inputText} onChange={inputTextHandler}
-                type="text" className="todo-input" required />
-            <button className="todo-button" type="submit">
-                <i className="fas fa-plus-square"></i>
-            </button>
-        </form>
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+  const SubmitTodoHandler = (e) => {
+    e.preventDefault();
 
-    );
-}
+    setTodos([
+      ...todos,
+      { text: inputText, completed: false, id: Math.random() * 1000 },
+    ]);
+    setInputText("");
+  };
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  return (
+    <form onSubmit={SubmitTodoHandler}>
+      <input
+        id="myText"
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+        required
+      />
+      <button className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </form>
+  );
+};
 
 export default Form;
